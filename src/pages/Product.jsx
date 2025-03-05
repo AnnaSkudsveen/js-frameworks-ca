3defa922d4b25e0aea55dddab8355ca8c73f3379
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-03-05 22:46:09 +0100

    feat: show single product from id

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Product() {
-  return <div>CartProduct</div>;
+  const [product, setProduct] = useState(null);
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function getProduct(url) {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setProduct(json.data);
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    }
+    getProduct(`https://v2.api.noroff.dev/online-shop/${id}`);
+  }, [id]);
+
+  if (!product) {
+    return <p>Loading products...</p>;
+  }
+
+  console.log(product);
+
+  return (
+    <div>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+      <img src={product.image.url} alt={product.image.alt} />
+      <p>Price: {product.price} kr</p>
+      <button>Add to Cart</button>
+    </div>
+  );
 }
 
 export default Product;
