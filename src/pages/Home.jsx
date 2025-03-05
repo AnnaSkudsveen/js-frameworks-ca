e958d044069ddc2ee7e539a11099d02f87bc7a91
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-03-05 22:45:51 +0100

    feat: show all products on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../components/Layout/Button";
+
+function ProductCard() {
+  const url = "https://v2.api.noroff.dev/online-shop/";
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setProducts(json.data);
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    }
+    getData();
+  }, []);
+
+  if (!products) {
+    return <p>Loading products...</p>;
+  }
+
+  return (
+    <>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <img
+            src={product.image.url}
+            alt={product.image.alt}
+            className="productImage"
+          />
+          <p>Price: {product.price} kr</p>
+          <Link to={`/product/${product.id}`}>View product</Link>
+        </div>
+      ))}
+    </>
+  );
+}
+
 function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <img src="https://github.com/annaskudsveen.png" alt="" />
+      <ProductCard />
     </div>
   );
 }
