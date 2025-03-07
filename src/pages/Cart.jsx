d1cd9deaf3535620e6d9d09ba5b83e66fd61c54e
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-03-07 23:38:51 +0100

    feat: cart logic to get products from cart and view on page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,37 @@
+import React from "react";
+import useStoreCart from "../components/store/cartStore";
+import { Link } from "react-router-dom";
+
 function Cart() {
-  return <div>Cart</div>;
+  const { cart, removeFromCart } = useStoreCart();
+  return (
+    <div>
+      <h1>In your cart</h1>
+      {cart.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div>
+          {cart.map(function (product) {
+            return (
+              <div key={product.id}>
+                <h2>{product.title}</h2>
+                <p>Total Price: {product.price * product.quantity} kr</p>
+                <p>Quantity: {product.quantity}</p>
+                <button
+                  onClick={function () {
+                    removeFromCart(product.id);
+                  }}
+                >
+                  Remove from cart
+                </button>
+              </div>
+            );
+          })}
+          <Link to="/checkout">Check out</Link>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Cart;
