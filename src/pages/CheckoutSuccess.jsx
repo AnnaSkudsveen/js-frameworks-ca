fa69cda554d2ffa1568d712e985f548eb0127489
AnnaSkudsveen <annaskudsveen@hotmail.com>
2025-03-09 20:50:13 +0100

    fix: fixed bug causing issues with checkout

--- src/pages/CheckoutSuccess.jsx
+++ src/pages/CheckoutSuccess.jsx
@@ -14,13 +14,21 @@ function CheckoutSuccess() {
   return (
     <div>
       <h1>Checkout Successful</h1>
-      {cart.map(function (product) {
-        <div key={product.id}>
-          <h2>{product.title}</h2>
-          <p>Total Price: {product.price * product.quantity || 1} kr</p>
-          <p>Quantity: {product.quantity || 1}</p>
-        </div>;
-      })}
+
+      {cart.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        cart.map((product) => {
+          return (
+            <div key={product.id}>
+              <h2>{product.title}</h2>
+              <p>Total Price: {product.price * (product.quantity || 1)} kr</p>
+              <p>Quantity: {product.quantity || 1}</p>
+            </div>
+          );
+        })
+      )}
+
       <button onClick={handleHome}>Exit</button>
     </div>
   );
